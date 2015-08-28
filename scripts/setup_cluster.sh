a80da51de0c1e4d70d479484d9f1a0e866bcdd1d
Rok Roskar <roskarr@ethz.ch>
2015-08-28 11:28:19 +0200

    changed cluster setup to use the miniconda in /cluster/apps

--- scripts/setup_cluster.sh
+++ scripts/setup_cluster.sh
@@ -1,25 +1,9 @@
 #!/bin/sh
 
 echo "setting up cluster python environment"
-	# install python
-	if [ ! -d ~/miniconda ]; then
-		cd ~
-		echo "installing python"
-		wget -nv https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh
-		chmod u+x Miniconda-latest-Linux-x86_64.sh
-		./Miniconda-latest-Linux-x86_64.sh -b -p ~/miniconda
-		rm Miniconda-latest-Linux-x86_64.sh
-		echo "prepending ~/miniconda/bin to PATH"
-		export PATH=~/miniconda/bin:$PATH
-		conda update -y conda
-		conda create -y -n spark_workshop numpy pip ipython jupyter matplotlib beautiful-soup bokeh pandas scipy
-		source activate spark_workshop
-		pip install findspark
-	fi
-	
-	echo "removing everything from the PYTHONPATH"
-	unset PYTHONPATH
-
-	export PATH=~/miniconda/bin:$PATH	
-	source activate spark_workshop
+echo "prepending /cluster/apps/spark/miniconda/bin to PATH"
+export PATH=/cluster/apps/spark/miniconda/bin:$PATH
+source activate spark_workshop
+echo "removing everything from the PYTHONPATH"
+unset PYTHONPATH
 
